6848dcfb7b7a22dd5503f0769ffdeff07e09550e
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-05-26 11:07:42 -0300

    Criando Interface NewGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,24 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Button } from '@components/Button';
+
+import { Container, Content, Icon } from './styles';
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+      </Content>
+
+      <Button title="Criar" />
+    </Container>
+  );
+}
